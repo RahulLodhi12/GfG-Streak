ff0d7ae041ba2d37e8edfda6e0344a005e972e8b
Rahul Lodhi <111701793+RahulLodhi12@users.noreply.github.com>
2024-09-27 13:10:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -1,6 +1,5 @@
 //{ Driver Code Starts
-// Initial template for C++
-
+// Initial Template for C++
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -11,26 +10,29 @@ using namespace std;
 class Solution {
   public:
     // Function to find maximum of each subarray of size k.
-    vector<int> max_of_subarrays(vector<int> &arr, int k) {
+    vector<int> max_of_subarrays(int k, vector<int> &arr) {
         // your code here
-        vector<int> ans;
         list<int> l;
         int n=arr.size();
         int i=0;
         int j=0;
+        vector<int> ans;
         while(j<n){
             while(!l.empty() and l.back()<arr[j]){
                 l.pop_back();
             }
             l.push_back(arr[j]);
-            if(j-i+1==k){
+            if(j-i+1<k){
+                j++;
+            }
+            else if(j-i+1==k){
                 ans.push_back(l.front());
                 if(arr[i]==l.front()){
                     l.pop_front();
                 }
                 i++;
+                j++;
             }
-            j++;
         }
         return ans;
     }
@@ -42,17 +44,23 @@ int main() {
 
     int t;
     cin >> t;
+    cin.ignore();
 
     while (t--) {
 
-        int n, k;
-        cin >> n >> k;
-
-        vector<int> arr(n);
-        for (int i = 0; i < n; i++)
-            cin >> arr[i];
-        Solution ob;
-        vector<int> res = ob.max_of_subarrays(arr, k);
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> res = obj.max_of_subarrays(k, arr);
         for (int i = 0; i < res.size(); i++)
             cout << res[i] << " ";
         cout << endl;
@@ -60,4 +68,5 @@ int main() {
 
     return 0;
 }
+
 // } Driver Code Ends
