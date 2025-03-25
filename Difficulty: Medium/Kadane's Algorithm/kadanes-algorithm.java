399e41c98ee78ccb5434768ce05fb7aa7e3c4c85
Rahul Lodhi <111701793+RahulLodhi12@users.noreply.github.com>
2025-03-25 23:08:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            Solution obj = new Solution();
+
+            // calling maxSubarraySum() function
+            System.out.println(obj.maxSubarraySum(arr));
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    int maxSubarraySum(int[] arr) {
+        // Your code here
+        int n=arr.length;
+        int i=0,j=0;
+        
+        int sum=0;
+        int maxi=Integer.MIN_VALUE;
+        
+        while(j<n){
+            sum+=arr[j];
+            maxi=Math.max(maxi,sum);
+            if(sum<0){
+                sum=0;
+            }
+            j++;
+        }
+        return maxi;
+    }
+}
