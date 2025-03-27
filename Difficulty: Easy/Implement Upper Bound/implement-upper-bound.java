c100445c2f3b0c13d489bbd2e8ea5c5d27dc4226
Rahul Lodhi <111701793+RahulLodhi12@users.noreply.github.com>
2025-03-27 19:36:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Upper Bound/implement-upper-bound.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+            String[] nk = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(nk[0]);
+            Solution sln = new Solution();
+            int ans = sln.upperBound(a, k);
+
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    int upperBound(int[] nums, int x) {
+        // code here
+        int n=nums.length;
+        int s=0,e=n-1;
+
+        int idx=n; //ans;
+
+        while(s<=e){
+            int m = s + (e-s)/2;
+            if(nums[m]<=x){
+                s=m+1;
+            }
+            else if(nums[m]>x){ //ans will be here
+                idx = Math.min(idx,m);
+                e=m-1;
+            }
+        }
+
+        return idx;
+    }
+}
