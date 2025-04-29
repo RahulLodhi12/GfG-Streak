babfbfe6f8774886c8acbd9e2e2cb1e32d52323c
Rahul Lodhi <111701793+RahulLodhi12@users.noreply.github.com>
2025-04-29 09:40:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.java
+++ Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.java
@@ -34,23 +34,62 @@ class GFG {
 // User function Template for Java
 
 class Solution {
+    int upperBound(int[] nums, int x){
+        int n=nums.length;
+        int s=0,e=n-1;
+
+        int idx=n; //ans;
+
+        while(s<=e){
+            int m = s + (e-s)/2;
+            if(nums[m]<=x){
+                s=m+1;
+            }
+            else if(nums[m]>x){ //ans will be here
+                idx = Math.min(idx,m);
+                e=m-1;
+            }
+        }
+
+        return idx;
+    }
+    int cntSmallEqual(int mat[][], int mid, int n, int m){
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            cnt += upperBound(mat[i],mid);
+        }
+        return cnt;
+    }
     int median(int mat[][]) {
         // code here
-        ArrayList<Integer> lst = new ArrayList<>();
+        int s=Integer.MAX_VALUE;
+        int e=Integer.MIN_VALUE;
         
         int n=mat.length;
         int m=mat[0].length;
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                lst.add(mat[i][j]);
+                s=Math.min(s,mat[i][j]);
+                e=Math.max(e,mat[i][j]);
             }
         }
         
-        Collections.sort(lst);
+        int req = (n*m)/2;
         
-        int size = (n*m)/2;
+        while(s<=e){
+            int mid = s + (e-s)/2; //median
+            
+            int smallEqual = cntSmallEqual(mat,mid,n,m);
+            
+            if(req<smallEqual){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+        }
         
-        return lst.get(size);
+        return s;
     }
 }
